6c8c108fb4713f0cbe24bac2f0a8f0aa518c8ec0
rafkrawczyk <54412881+rafkrawczyk@users.noreply.github.com>
2026-02-23 23:52:31 +0100

    Add files via upload

---
+++ ChromaMexilLogin.java
@@ -0,0 +1,55 @@
+package com.rafal;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Test {
+    public static void main(String[] args) {
+        WebDriver chromeDriver = new ChromeDriver();
+        //WebDriver firefoxDriver = new FirefoxDriver();
+        //WebDriver edgeDriver = new EdgeDriver();
+        
+        String url = "https://chroma.mexil.it/site/login";
+        
+        chromeDriver.manage().window().maximize();
+        chromeDriver.manage().deleteAllCookies();
+        chromeDriver.get(url);
+        
+        WebElement forgotPasswordLink = chromeDriver.findElement(By.linkText("Forgot Password?"));
+        forgotPasswordLink.click();
+        
+        WebElement emailField = chromeDriver.findElement(By.xpath("//*[@id='form-username']"));
+        emailField.sendKeys("general@teacher.com");
+        
+        WebElement adminLogin = chromeDriver.findElement(By.partialLinkText("Admin Log"));
+        adminLogin.click();
+        
+        WebElement userField = chromeDriver.findElement(By.id("form-username"));
+        userField.sendKeys("general@teacher.com");
+        
+        //WebElement passwordField = chromeDriver.findElement(By.name("password"));
+        //WebElement passwordField = chromeDriver.findElement(By.cssSelector("#form-password"));
+        
+        WebElement passwordField = chromeDriver.findElement(By.cssSelector("[class='form-password form-control']"));
+        passwordField.sendKeys("123456");        
+        
+        WebElement loginButton = chromeDriver.findElement(By.className("btn"));
+        loginButton.click();
+        
+        chromeDriver.quit();
+        
+        /*firefoxDriver.manage().window().maximize();
+        firefoxDriver.manage().deleteAllCookies();        
+        firefoxDriver.get(url);
+        firefoxDriver.quit();
+        
+        edgeDriver.manage().window().maximize();
+        edgeDriver.manage().deleteAllCookies();
+        edgeDriver.get(url);
+        edgeDriver.quit();*/
+    }    
+}
