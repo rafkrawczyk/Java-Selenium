de813fdf86bd20b0200f3504022296356db64fa5
rafkrawczyk <54412881+rafkrawczyk@users.noreply.github.com>
2026-04-10 18:07:31 +0200

    Add files via upload

---
+++ DoWhile.java
@@ -0,0 +1,14 @@
+package FirstJavaProgram;
+
+public class DoWhile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int j = 0;
+		
+		do {
+			System.out.println(j);
+			j++;
+		} while (j < 5);
+	}
+}
