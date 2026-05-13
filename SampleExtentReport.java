c7dc7ce748639e817815c2ee9b4b83c035820ffe
rafkrawczyk <54412881+rafkrawczyk@users.noreply.github.com>
2026-05-13 14:09:37 +0200

    Add files via upload

---
+++ SampleExtentReport.java
@@ -0,0 +1,36 @@
+package org.TestNG_Maven;
+
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+
+public class SampleExtentReport {
+	
+	@Test
+	public void sampleReport() {
+		
+		ExtentReports extent = new ExtentReports();
+		ExtentSparkReporter spark = new ExtentSparkReporter("my_sample_report.html");
+		spark.config().setTheme(Theme.DARK);
+		spark.config().setReportName("My_Sample_Report");
+		spark.config().setDocumentTitle("My First Extent Report");
+		extent.attachReporter(spark);
+		
+		ExtentTest loginTest = extent.createTest("Instagram Login Test");
+		loginTest.info("Passing User Name");
+		loginTest.info("Passing Password");
+		loginTest.info("Click Submit Button");
+		loginTest.pass("Test Success");
+		
+		ExtentTest homePageTest = extent.createTest("Instagram Homepage Test");
+		homePageTest.pass("Enter Instagram Homepage");
+		homePageTest.info("Click on Profile");
+		homePageTest.fail("Test Failed");
+		
+		extent.flush();
+	}
+
+}
